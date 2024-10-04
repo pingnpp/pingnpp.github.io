9f68f31926c9bd4771f265f7d5b4f4c09be8e709
ping_arv <noppornb@arv.co.th>
2024-10-04 14:40:16 +0700

    Updates

--- js/793.1efbb206.js
+++ js/793.1efbb206.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkpingnpp_github_io"]=self["webpackChunkpingnpp_github_io"]||[]).push([[793],{6793:function(n,u,t){t.r(u),t.d(u,{default:function(){return c}});var i=t(641);const a={class:"about"};function e(n,u){return(0,i.uX)(),(0,i.CE)("div",a,u[0]||(u[0]=[(0,i.Lk)("h1",null,"This is an about page",-1)]))}var r=t(6262);const s={},p=(0,r.A)(s,[["render",e]]);var c=p}}]);
-//# sourceMappingURL=793.a04a23ed.js.map
+//# sourceMappingURL=793.1efbb206.js.map
